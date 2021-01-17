67d8a091b42d43f1807a71b28c8c5a1a44b7db2f
Frankie Chiappetta <fachiappetta@gmail.com>
2021-01-17 11:35:28 -0600

    Plot 1: Histogram of Global Active Power
    Final commit for script & png of Plot1

--- Plot1.R
+++ Plot1.R
@@ -1,4 +1,4 @@
-setwd("~/Desktop/DS/R STUDIO/exp_data")
+setwd("/Users/frankiechiappetta/Documents/GitHub/household_energy_consumption")
 
 library(plyr)
 library(dplyr)
@@ -30,7 +30,7 @@ febdata$Time <- as.POSIXct(datetime)
 febdata$Date <- as.Date(febdata$Date, "%d/%m/%y")
 
 #create histogram
-png(file = "Plot1.png")
+png(file = "Plot1.png", width=480, height=480)
 hist(febdata$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
 dev.off()
 
