daf22711b799bd12fa7f951b836bbe48c38fadfe
Frankie Chiappetta <fachiappetta@gmail.com>
2021-01-17 10:22:20 -0600

    revised Plot1
    revised timedate in plot 1

--- Plot1.R
+++ Plot1.R
@@ -20,13 +20,16 @@ columnnames <- colnames(read.table("./data/household_power_consumption.txt", nro
 #dataset <- read.table("/Users/frankiechiappetta/Desktop/DS/R STUDIO/exp_data/data/household_power_consumption.txt", header=TRUE, sep = ";")
 febdata <- fread("grep -E '^1/2/2007|^2/2/2007' ./data/household_power_consumption.txt", col.names = (columnnames), na.strings="?")
 
+#convert time to time class
+datetime <- strptime(paste(febdata$Date, febdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+#convert time to time class
+febdata$Time <- as.POSIXct(datetime)
+
 #convert date to date class
 febdata$Date <- as.Date(febdata$Date, "%d/%m/%y")
 
-#convert time to time class
-time <- as.POSIXct(febdata$Time, format = "%H:%M:%S")
-febdata$Time <- time
-
+#create histogram
 png(file = "Plot1.png")
 hist(febdata$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
 dev.off()
